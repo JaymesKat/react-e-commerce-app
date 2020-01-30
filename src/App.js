94e7561b8320e0700a799c166ea997d88fca46a1
James Katarikawe <james.katarikawe@andela.com>
2020-01-30 14:04:28 +0300

    add routing and menu item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (<h1>Hats Page</h1>)
 class App extends Component {
   render() {
-    return <div><Homepage/></div>;
-  }
+    return (
+			<div>
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
+			</div>
+		);}
 }
 
 export default App;

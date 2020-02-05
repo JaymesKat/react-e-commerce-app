07d2083398a4ab1b55f82b21721b0ef8c28d8900
James Katarikawe <james.katarikawe@andela.com>
2020-02-05 11:07:28 +0300

    added shop page, collection preview and collection item components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
-import Homepage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (<h1>Hats Page</h1>)
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 class App extends Component {
   render() {
     return (
 			<div>
         <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
 			</div>
 		);}
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./homepage.styles.scss";
-import Directory from '../../components/directory/directory.component.jsx';
+import Directory from '../../components/directory/directory.component';
 
-const Homepage = () => (
+const HomePage = () => (
 	<div className='homepage'>
         <Directory/>
     </div>
 );
 
-export default Homepage;
+export default HomePage;

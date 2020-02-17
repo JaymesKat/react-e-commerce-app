60a8be13aaccd8a67029bf79f0f107c3d84c41cc
James Katarikawe <james.katarikawe@andela.com>
2020-02-17 08:06:49 +0300

    Move shop state to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
-
+import shopReducer from "./shop/shop.reducer";
 import directoryReducer from "./directory/directory.reducer";
 
 import { persistReducer } from "redux-persist";
@@ -9,15 +9,16 @@ import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
 	key: "root",
-    storage,
-    whitelist: ['cart']
+	storage,
+	whitelist: ["cart"]
 };
 
 export default persistReducer(
-    persistConfig,
+	persistConfig,
 	combineReducers({
 		user: userReducer,
 		cart: cartReducer,
-		directory: directoryReducer
+		directory: directoryReducer,
+		shop: shopReducer
 	})
 );

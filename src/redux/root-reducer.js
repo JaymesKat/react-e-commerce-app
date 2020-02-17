664a49ab86a626aa24c27f7876d014dd512335f7
James Katarikawe <james.katarikawe@andela.com>
2020-02-17 07:52:32 +0300

    Move directory component state to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
 
+import directoryReducer from "./directory/directory.reducer";
+
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
@@ -15,6 +17,7 @@ export default persistReducer(
     persistConfig,
 	combineReducers({
 		user: userReducer,
-		cart: cartReducer
+		cart: cartReducer,
+		directory: directoryReducer
 	})
 );

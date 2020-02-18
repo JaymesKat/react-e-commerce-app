81a3382961381729fe0b2951455f8d29ea68e3a2
James Katarikawe <james.katarikawe@andela.com>
2020-02-18 09:24:52 +0300

    integrate stripe payment with react-stripe-checkout package

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,7 @@ import {
 	selectCartItems,
 	selectCartTotal
 } from "../../redux/cart/cart.selectors";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -26,6 +27,13 @@ const CheckoutPage = ({ cartItems, total }) => (
 		<div className='total'>
 			<span>Total: ${total}</span>
 		</div>
+			<div className='test-warning'>
+			* Please use the following test credit card for payment
+			<br/>
+			4242 4242 4242 4242 - Exp: 01/21 CVV: 123
+
+			</div>
+		<StripeCheckoutButton price={total} />
 	</div>
 );
 

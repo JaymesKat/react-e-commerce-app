754a81ce042530ba33e904c7b7ca69abba40406a
James Katarikawe <james.katarikawe@andela.com>
2020-03-02 23:01:44 +0300

    refactor Shop page with container pattern

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,17 @@
+import { connect } from "react-redux";
+import { compose } from "redux";
+import { createStructuredSelector } from "reselect";
+import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+import  CollectionsOverview  from "./collections-overview.component";
+import  WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const mapStateToProps = createStructuredSelector({
+	isLoading: selectIsCollectionFetching
+});
+
+const CollectionsOverviewContainer = compose(
+	connect(mapStateToProps),
+	WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
